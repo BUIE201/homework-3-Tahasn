55be52c86a31fa33a7142e502f52d6cc555b6af0
Tahasn <tahasonmez98@hotmail.com>
2021-01-25 15:10:47 +0300

    Tree code pulled

--- Source.cpp
+++ Source.cpp
@@ -1,3 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+struct Node
+{
+	int i;
+		Node* pLeft;
+		Node* pRight;
+
+		Node(int iIn) : i(iIn), pLeft(nullptr), pRight(nullptr){}
+};
+
+void InsertToTree(Node*& pRoot, Node* pNew)
+{
+	if (!pRoot)
+	{
+		pRoot = pNew;
+		return;
+	}
+
+	if (pNew->i <= pRoot->i)
+		InsertToTree(pRoot->pLeft, pNew);
+	else
+		InsertToTree(pRoot->pRight, pNew);
+}
+void PrintTree(Node* pRoot, int Level)
+{
+	if (!pRoot)
+		return;
+
+	PrintTree(pRoot->pRight, Level + 1);
+
+	for (int i = 0; i < Level; i++)
+		cout << "  ";
+	cout << pRoot->i << endl;
+
+	PrintTree(pRoot->pLeft, Level + 1);
+}
 
 void main()
 {
